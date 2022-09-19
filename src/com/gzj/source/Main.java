acca5c94cd0572084003e8376e6496d7bd289595
guozijie <maidi_guo2014@163.com>
2022-09-19 23:48:58 +0800

    this my first commit of myeclipse

---
+++ src/com/gzj/source/Main.java
@@ -0,0 +1,10 @@
+package com.gzj.source;
+
+public class Main {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println("this is my first git java");
+	}
+
+}
