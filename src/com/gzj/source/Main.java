312b53818e7c1967dfcaa9208685abe74b87f82b
guozijie <maidi_guo2014@163.com>
2022-09-20 00:03:19 +0800

    this is second commit

--- src/com/gzj/source/Main.java
+++ src/com/gzj/source/Main.java
@@ -5,6 +5,11 @@ public class Main {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		System.out.println("this is my first git java");
+		
+		System.out.println("20220920");
+		
+		System.out.println("202209011s");
+		
 	}
 
 }
